77906e25ea5385203c72c541f11589fbb611490a
Davi Alexandre <davi@davialexandre.com.br>
2011-10-02 23:12:10 -0300

    Updating the menu and removing unused actions

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -28,11 +28,9 @@
 	<div id="mainmenu">
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
-				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
+				array('label'=>'Yiinfinite-Scroll', 'url'=>array('yiinfinite_scroll/index')),
+				array('label'=>'Simple Calendar', 'url'=>array('simple_calendar/index')),
+                array('label' => 'Contact', 'url'=>array('site/contact')),
 			),
 		)); ?>
 	</div><!-- mainmenu -->
