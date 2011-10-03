bb9dca11658e00a9e4447cfe3bbd607190908cb7
Davi Alexandre <davi@davialexandre.com.br>
2011-10-02 22:49:25 -0300

    Updating index to load dev.php config file and use framework path from .framework_path

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <?php
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../../../../Users/davi/Desenvolvimento/frameworks/yii/yii-1.1.8.r3324/framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
-
+$yii=trim(file_get_contents('.framework_path')).'/yii.php';
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
 require_once($yii);
+
+$config=dirname(__FILE__).'/protected/config/main.php';
+if(file_exists(dirname(__FILE__).'/protected/config/dev.php')) {
+    $devconfig = require_once(dirname(__FILE__).'/protected/config/dev.php');
+    $config = require_once($config);
+    $config = CMap::mergeArray($config, $devconfig);
+}
+
 Yii::createWebApplication($config)->run();
