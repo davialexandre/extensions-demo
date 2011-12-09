535cd2ec18bd24ee7a2a65070b3bebc30be9c8a8
Davi Alexandre <davi@davialexandre.com.br>
2011-12-09 16:46:04 -0200

    adding google analytics code

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -20,6 +20,19 @@
     <?php Yii::app()->clientScript->registerScript('colorbox', '$(".colorbox").colorbox({maxHeight: "80%"})', CClientScript::POS_READY); ?>
     <?php Yii::app()->clientScript->registerScript('extension-information', '$(".extension-information").stickyPanel()', CClientScript::POS_READY); ?>
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+    <script type="text/javascript">
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-4650336-1']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    </script>
 </head>
 
 <body>
