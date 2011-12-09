b420a8130381c1f4642dc3732588b20fab80977b
Davi Alexandre <davi@davialexandre.com.br>
2011-12-09 16:38:56 -0200

    updating the home page text

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,16 +1,10 @@
 <?php $this->pageTitle=Yii::app()->name; ?>
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+<h1>Hi!</h1>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
-
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <tt><?php echo __FILE__; ?></tt></li>
-	<li>Layout file: <tt><?php echo $this->getLayoutFile('main'); ?></tt></li>
-</ul>
-
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+<p>
+    I'm <a href="http://davialexandre.com.br">Davi Alexandre</a>. 
+    I'm a PHP developer, and I've been working with <a href="http://yiiframework.com">Yii Framework</a> since 2009.
+</p>
+<p>Here you will find demos and sample codes for use with this cool framework!.</p>
+<p>Use the menu above to see the demos. Enjoy!</p>
